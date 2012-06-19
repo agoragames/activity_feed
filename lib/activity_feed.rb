cf2db14f4a4a551ce87c5508a5c296650f270271
David Czarnecki <czarneckid@acm.org>
2012-06-18 21:45:08 -0400

    feed_key method and spec

--- lib/activity_feed.rb
+++ lib/activity_feed.rb
@@ -3,4 +3,10 @@ require 'activity_feed/configuration'
 
 module ActivityFeed
   extend Configuration
+
+  def self.feed_key(user_id, aggregate = false)
+    aggregate ? 
+      "#{ActivityFeed.namespace}:#{ActivityFeed.aggregate_key}:#{user_id}" :
+      "#{ActivityFeed.namespace}:#{user_id}"
+  end
 end
