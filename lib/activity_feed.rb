d71e66631728190a095e5ca54b9a18727f7e009c
David Czarnecki <czarneckid@acm.org>
2012-06-18 21:54:03 -0400

    Added Utility module and spec

--- lib/activity_feed.rb
+++ lib/activity_feed.rb
@@ -1,12 +1,8 @@
 require 'activity_feed/version'
 require 'activity_feed/configuration'
+require 'activity_feed/utility'
 
 module ActivityFeed
   extend Configuration
-
-  def self.feed_key(user_id, aggregate = false)
-    aggregate ? 
-      "#{ActivityFeed.namespace}:#{ActivityFeed.aggregate_key}:#{user_id}" :
-      "#{ActivityFeed.namespace}:#{user_id}"
-  end
+  extend Utility  
 end
