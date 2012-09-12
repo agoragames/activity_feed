ffd8e7eddbd16cbbeaf3c7cb4b2eea482c7181a2
David Czarnecki <me@davidczarnecki.com>
2012-09-12 11:28:14 -0400

    Bump version to 2.2.2.

--- lib/activity_feed/version.rb
+++ lib/activity_feed/version.rb
@@ -1,3 +1,3 @@
 module ActivityFeed
-  VERSION = "2.2.1"
+  VERSION = "2.2.2"
 end
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'ActivityFeed::VERSION' do
   it "should be the correct version" do
-    ActivityFeed::VERSION.should == '2.2.1'
+    ActivityFeed::VERSION.should == '2.2.2'
   end
 end
